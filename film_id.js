4e2ea0ca124a62affb4997ff7021f152fc83f8a2
valdiss <valentin.dissais@gmail.com>
2017-02-17 16:58:29 +0100

    fusion branche val et mohammed

--- film_id.js
+++ film_id.js
@@ -1,16 +1,27 @@
 var app = angular.module('movieplug', []);
 app.controller('film_id', function($scope, $http) {
     $http.get("https://api.themoviedb.org/3/genre/878/movies?api_key=e72f880255c5b01996f688fc51152f3a").
-      then(function(response) {
+    then(function(response) {
         $scope.films = response.data;
         $scope.film_list = $scope.films.results;
     });
     $scope.show_infos = function(i) {
-      $scope.title = i.original_title;
-      $scope.overview = i.overview;
-      $scope.date = i.release_date;
-      $scope.img = "https://image.tmdb.org/t/p/w640"+i.poster_path;
-      $scope.vote = i.vote_average;
-      console.log($scope.img);
-   };
+        $scope.title = i.original_title;
+        $scope.overview = i.overview;
+        $scope.date = i.release_date;
+        $scope.img = "https://image.tmdb.org/t/p/w640" + i.poster_path;
+        $scope.vote = i.vote_average;
+        console.log($scope.img);
+
+        let request = i.original_title + ' trailer';
+        console.log(request);
+
+        $http.get('https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + request + '&key=AIzaSyBo6GWJnzMlR_M4WQOQGYBcZ2lU_Lgw2LQ').then(function(response) {
+            let filmList = response.data;
+            $scope.videoId = filmList.items[0].id.videoId;
+        });
+
+    };
+
+
 });
