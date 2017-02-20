b2c62ef6c72d332acf760ccda15b5116d7cc4ce8
valdiss <valentin.dissais@gmail.com>
2017-02-20 15:16:25 +0100

    searchbar working

---
+++ film_id.js
@@ -0,0 +1,41 @@
+var app = angular.module('movieplug', []);
+app.controller('film_id', function($scope, $http) {
+    $http.get("https://api.themoviedb.org/3/genre/878/movies?api_key=e72f880255c5b01996f688fc51152f3a").
+    then(function(response) {
+        $scope.films = response.data;
+        $scope.film_list = $scope.films.results;
+    });
+
+    $http.get("https://api.themoviedb.org/3/genre/878/movies?api_key=e72f880255c5b01996f688fc51152f3a&page=2").
+    then(function(response) {
+        $scope.films = response.data;
+        $scope.film_list2 = $scope.films.results;
+    });
+    $http.get("https://api.themoviedb.org/3/genre/878/movies?api_key=e72f880255c5b01996f688fc51152f3a&page=3").
+    then(function(response) {
+        $scope.films = response.data;
+        $scope.film_list3 = $scope.films.results;
+    });
+
+    $scope.show_infos = function(i) {
+      console.log("click is ok !");
+
+        $scope.title = i.original_title;
+        $scope.overview = i.overview;
+        $scope.date = i.release_date;
+        $scope.img = "https://image.tmdb.org/t/p/w640" + i.poster_path;
+        $scope.vote = i.vote_average;
+        console.log($scope.img);
+
+        let request = i.original_title + ' trailer';
+        console.log(request);
+
+        $http.get('https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + request + '&key=AIzaSyBo6GWJnzMlR_M4WQOQGYBcZ2lU_Lgw2LQ').then(function(response) {
+            let filmList = response.data;
+            $scope.videoId = filmList.items[0].id.videoId;
+        });
+
+    };
+
+
+});
